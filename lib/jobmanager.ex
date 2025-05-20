098a10a6ef751c57d55f05d30325704a2215a972
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-20 17:11:23 +0200

    Idea inicial del supervisor de tareas

--- lib/jobmanager.ex
+++ lib/jobmanager.ex
@@ -1,3 +1,34 @@
 defmodule JobManager do
+  use Supervisor
+
+  @impl Supervisor
+  def init(_init_arg) do
+    opts = [
+      strategy: :simple_one_for_one,
+      max_restarts: 1,
+      max_seconds: 5
+    ]
+
+    Supervisor.init([], opts);
+  end
+
+  def start_link() do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  def start_job(job_state) do
+    child_spec = [
+      %{
+        id: job_state[:id],
+        start: {Job, :start_link, [job_state]},
+        restart: :transient,
+        type: :worker
+      }
+    ]
+
+    Supervisor.start_child(__MODULE__, child_spec)
+  end
+
+
 
 end
