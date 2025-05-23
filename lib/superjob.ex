eaa53e5e5aaff40064c21843bfd23f80d9e4f95a
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-23 10:36:14 +0200

    Rebase con new-working-mode

--- lib/superjob.ex
+++ lib/superjob.ex
@@ -1,39 +1,19 @@
 defmodule SuperJob do
   use Supervisor
 
-  @impl Supervisor
-  def init(info) do
+   @impl Supervisor
+  def init(_init_arg) do
     opts = [
       strategy: :one_for_one,
       max_restarts: 1,
       max_seconds: 5
     ]
 
-    workers =
-      if(Map.get(info, :num_workers, :unbound) != :unbound) do
-        for i <- 1..Map.get(info, :num_workers, 1) do
-          %{
-            id: :"worker_#{i}",
-            start: {Worker, :start_link, [[name: :"worker_#{i}"]]}
-          }
-        end
-      else
-        []
-      end
-
-    Supervisor.init(workers, opts)
+    Supervisor.init([], opts)
   end
 
   def start_link(opts) do
-    Supervisor.start_link(
-      __MODULE__,
-      %{
-        num_workers: Keyword.get(opts, :num_workers, :unbound),
-        free_workers: :queue.new(),
-        processing_jobs: %{}
-      },
-      name: JobManager
-    )
+    Supervisor.start_link(__MODULE__, [], opts)
   end
 
   def start_job(job_state) do
@@ -47,20 +27,4 @@ defmodule SuperJob do
     Supervisor.start_child(__MODULE__, child_spec)
   end
 
-  def worker_ready() do
-    GenServer.call(JobManager, {:ready, {Process.info(self())[:refgistered_name], self()}})
-  end
-
-  def task_completed(name, result, worker_name) do
-    GenServer.call(JobManager, {:ended_task, name, result, worker_name})
-  end
-
-  def handle_call({:ready, {name, pid}}, _, state) do
-    :queue.in(%{name: name, pid: pid}, state[:free_workers])
-    {:reply, :ok}
-  end
-
-  def handle_call({:ended_task, name, result, worker_name}) do
-    ## Maneja la tarea
-  end
 end
