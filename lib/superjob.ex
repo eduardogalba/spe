6eacea1aad6dccee2a1058ce35189861b82baacc
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-23 13:47:10 +0200

    Agregada la logica para el gestor de workers en el supervisor de trabajos

--- lib/superjob.ex
+++ lib/superjob.ex
@@ -2,29 +2,37 @@ defmodule SuperJob do
   use Supervisor
 
    @impl Supervisor
-  def init(_init_arg) do
+  def init(children) do
     opts = [
       strategy: :one_for_one,
       max_restarts: 1,
       max_seconds: 5
     ]
 
-    Supervisor.init([], opts)
+    Supervisor.init(children, opts)
   end
 
-  def start_link() do
-    Supervisor.start_link(__MODULE__, [], name: SPE.SuperJob)
-  end
-
-  def start_job(job_state) do
-    child_spec =
+  def start_link(job_state, num_workers) do
+    job =
       %{
         id: job_state[:id],
         start: {Job, :start_link, [job_state]},
         restart: :transient
       }
 
-    Supervisor.start_child(SPE.SuperJob, child_spec)
+    super_task =
+      %{
+        id: "st_" <> inspect(job_state[:id]),
+        start: {SuperTask, :start_link, [[num_workers: num_workers]]},
+        restart: :transient
+      }
+
+    children = [
+      job,
+      super_task
+    ]
+
+    Supervisor.start_link(__MODULE__, [children], name: SPE.SuperJob)
   end
 
 end
