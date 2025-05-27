e77b69406fb9597a9ad3527b1d9ad20741833713
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-27 20:56:17 +0200

    Configuraciones de conexion y schema de los estados de los trabajos

---
+++ lib/schemas/job.ex
@@ -0,0 +1,14 @@
+defmodule SPE.Schemas.Job do
+  use Ecto.Schema
+
+  @primary_key {:id, :string, autogenerate: false}
+  schema "jobs" do
+    field :plan, :map
+    field :num_workers, :integer
+    field :enables, :map
+    field :returns, :map
+    field :results, :map
+    field :tasks, :map
+    field :inserted_at, :utc_datetime
+  end
+end
