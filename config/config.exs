98f9a94a64aec871bd457368b84c250ebed42f3e
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-27 20:52:57 +0200

    Agregada configuracion con la base de datos

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,12 @@
 import Config
 
 config :logger, level: :info
+
+config :spe, SPE.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "spe",
+  hostname: "localhost",
+  pool_size: 10
+
+config :spe, ecto_repos: [SPE.Repo]
