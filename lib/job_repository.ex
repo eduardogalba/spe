d433a4fc997513b9597dae18f39bce6a9693fd6a
Eduardo Gil Alba <eduardo.gil.alba@alumnos.upm.es>
2025-05-28 14:25:10 +0200

    Agregado un serializador como capa intermedia antes de guardar en bd y despues de recuperar de bd

--- lib/job_repository.ex
+++ lib/job_repository.ex
@@ -1,64 +1,29 @@
 defmodule JobRepository do
   import Ecto.Query
 
-  def save_job_state(job_id, state) do
-    %{
-      plan: plan,
-      results: results,
-      returns: returns,
-      enables: enables,
-      num_workers: num_workers,
-      tasks: tasks
-    } = state
-
-    completed_tasks =
-      results
-      |> Enum.filter(fn {_, status} -> status != :not_run end)
-      |> Enum.map(fn {task, _} -> task end)
-
-    job_data = %{
-      id: job_id,
-      plan: Jason.encode!(plan),
-      completed_tasks: completed_tasks,
-      num_workers: num_workers,
-      enables: Jason.encode!(enables),
-      returns: Jason.encode!(returns),
-      results: Jason.encode!(results),
-      tasks: Jason.encode!(tasks)
-    }
+  def save_job_state(state) do
+    job_data = Serializer.serialize_job(state)
 
     job_struct = SPE.Schemas.Job |> struct(job_data)
 
     SPE.Repo.insert!(
       job_struct,
       on_conflict: :replace_all,
-      conflict_target: :id
+      conflict_target: :name
     )
   end
 
-  def get_job_state(job_id) do
-    case SPE.Repo.get(SPE.Schemas.Job, job_id) do
+  def get_job_state(job_name) do
+    case SPE.Repo.get_by(SPE.Schemas.Job, name: job_name) do
       nil -> {:error, :not_found}
       job ->
-        {:ok, %{
-          plan: Jason.decode!(job.plan),
-          results: Jason.decode!(job.results),
-          returns: Jason.decode!(job.returns),
-          enables: Jason.decode!(job.enables),
-          num_workers: job.num_workers,
-          tasks: Jason.decode!(job.tasks)
-        }}
+        {:ok, Serializer.deserialize_job(job)}
     end
   end
 
-  def delete_job(job_id) do
-    from(j in SPE.Schemas.Job, where: j.id == ^job_id)
+  def delete_job(job_name) do
+    from(j in SPE.Schemas.Job, where: j.name == ^job_name)
     |> SPE.Repo.delete_all()
   end
 
-  def list_unfinished_jobs() do
-    from(j in SPE.Schemas.Job,
-      where: fragment("? @> ?", j.results, ^Jason.encode!(%{"status" => "pending"})))
-    |> SPE.Repo.all()
-  end
 end
