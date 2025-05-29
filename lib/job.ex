fab67939d49159d910cae7570d9a9c4d9534b2ff
Night-Emperor-101 <al.ortega@alumnos.upm.es>
2025-05-29 10:10:43 +0000

    Generating docs for module job

--- lib/job.ex
+++ lib/job.ex
@@ -1,33 +1,110 @@
 defmodule Job do
+  @moduledoc """
+  **Job** module is responsible for managing the execution of tasks
+  in a distributed system. It handles task assignment to workers,
+  monitors their completion, and manages the overall state of the job.
+  It uses a GenServer to maintain the state and handle asynchronous
+  messages related to task execution and worker readiness.
+
+  #Functions:
+  - `start_link/1`: Starts the job with the initial state.
+  - `init/1`: Initializes the job state and starts the GenServer.
+  - `handle_cast/2`: Handles asynchronous messages for worker readiness and task completion.
+  - `handle_info/2`: Handles informational messages, such as worker notifications.
+  - `task_completed/2`: Notifies the job that a task has been completed.
+  - `worker_ready/2`: Notifies the job that a worker is ready to take tasks.
+  - `should_we_finish?/1`: Checks if all tasks are completed and decides whether to finish the job.
+  - `dispatch_tasks/1`: Dispatches tasks to available workers based on the current plan.
+
+  #Usage:
+  To use this module, you would typically start a job with `Job.start_link/1` and then
+  send tasks to it using `Job.task_completed/2` and `Job.worker_ready/2`.
+  It is designed to work with a distributed system where tasks can be executed by multiple workers,
+  and it handles the complexities of task dependencies and worker availability.
+  #Example:
+  ```elixir
+  {:ok, job_pid} = Job.start_link(%{id: "job_1", tasks: %{"task_1" => %{"exec" => "some_exec", "timeout" => 5000}}})
+  Job.worker_ready(job_pid, self())
+  Job.task_completed(job_pid, {"task_1", {:result, "task_result"}, self()})
+  ```
+  """
   use GenServer
   require Logger
 
+  @doc """
+  Initializes the job state when the GenServer starts.
+  This function sets up the initial state of the job, including
+  the task returns, ongoing tasks, free workers, results, and the start time.
+  It also logs the start of the job for debugging purposes.
+  # Parameters:
+  - `state`: The initial state of the job, which includes the job ID and tasks to be executed.
+  # Returns:
+  - `{:ok, state}`: Returns the initial state of the job wrapped in an `:ok` tuple.
+  # Example:
+  ```elixir
+  {:ok, initial_state} = Job.init(%{id: "job_1", tasks: %{"task_1" => %{"exec" => "some_exec", "timeout" => 5000}}})
+  ```
+  """
   def init(state) do
     Logger.debug("[Job #{inspect(self())}]: Job starting...")
     {:ok, state}
   end
 
+  @doc """
+  Starts the job with the given initial state.
+  This function initializes the job by setting up the necessary state
+  and prepares it to handle tasks and workers. It also logs the start of the
+  # Parameters:
+  - `state`: The initial state of the job, which includes the job ID and tasks to be executed.   Returns::
+  - `{:ok, pid}`: Returns the PID of the started job process wrapped in an `:ok` tuple.
+    mple:
+  :
+  ```elixir
+  {:ok, job_pid} = Job.start_link(%{id: "job_1", tasks: %{"task_1" => %{"exec" => "some_exec", "timeout" => 5000}
+  }})
+  ```
+  """
   def start_link(state) do
     Logger.debug("Iniciando trabajo...")
 
     new_state =
       state
-      |> Map.put(:returns, %{}) # Esto tambien sirve para los argumentos de las tareas
+      # Esto tambien sirve para los argumentos de las tareas
+      |> Map.put(:returns, %{})
       |> Map.put(:ongoing_tasks, [])
       |> Map.put(:free_workers, [])
       |> Map.put(:results, %{})
-      |> Map.put(:time_start, :erlang.monotonic_time(:millisecond)) # Empieza el cronometro
+      # Empieza el cronometro
+      |> Map.put(:time_start, :erlang.monotonic_time(:millisecond))
       |> Map.put(:busy_workers, %{})
 
     GenServer.start_link(__MODULE__, new_state, [])
   end
 
+  @doc """
+  Handles asynchronous messages related to worker readiness and task completion.
+  This function processes messages sent to the job, such as when a worker is ready
+  to take tasks or when a task has been completed by a worker. It updates the job state
+  accordingly and checks if the job should finish based on the current state.
+  # Parameters:
+  - `message`: The message to be processed, which can be either a worker readiness notification
+    or a task completion notification.
+  - `state`: The current state of the job, which includes ongoing tasks, free workers, and results.
+  # Returns:
+  - `{:noreply, new_state}`: Returns the updated state of the job wrapped in a `:noreply` tuple.
+  # Example:
+  ```elixir
+  GenServer.cast(job_pid, {:worker_ready, worker_pid})
+  GenServer.cast(job_pid, {:task_terminated, {task_name, {:result, value}, worker_pid}})
+  ```
+  """
   def handle_cast({:worker_ready, worker_pid}, state) do
     Logger.debug("[Job #{inspect(self())}]: Nuevo worker #{inspect(worker_pid)}...")
     free_workers = state[:free_workers] ++ [worker_pid]
     # Si se cae, me llegara una notificacion
     Process.monitor(worker_pid)
     Worker.are_u_ready?(worker_pid)
+
     new_state =
       state
       |> Map.put(:free_workers, free_workers)
@@ -38,8 +115,12 @@ defmodule Job do
   def handle_cast({:task_terminated, {task_name, {:result, value}, worker_pid}}, state) do
     Logger.debug("[Job #{inspect(self())}]: Handling task #{inspect(task_name)} ending...")
     new_ongoing_tasks = List.delete(state[:ongoing_tasks], task_name)
-    #Logger.debug("[Job #{inspect(self())}]: These are the tasks stil running => #{inspect(new_ongoing_tasks)}")
-    Logger.debug("[Job #{inspect(self())}]: Task Name: #{inspect(task_name)} Result: #{inspect(value)}")
+
+    # Logger.debug("[Job #{inspect(self())}]: These are the tasks stil running => #{inspect(new_ongoing_tasks)}")
+    Logger.debug(
+      "[Job #{inspect(self())}]: Task Name: #{inspect(task_name)} Result: #{inspect(value)}"
+    )
+
     new_returns =
       state[:returns]
       |> Map.put(task_name, value)
@@ -57,7 +138,7 @@ defmodule Job do
       |> Map.put(:busy_workers, Map.delete(state[:busy_workers], worker_pid))
 
     if new_ongoing_tasks == [] and Map.keys(state[:tasks]) do
-      #Logger.debug("[Job #{inspect(self())}]: Next plan floor = > #{inspect(state[:plan])}")
+      # Logger.debug("[Job #{inspect(self())}]: Next plan floor = > #{inspect(state[:plan])}")
       should_we_finish?(new_state)
     else
       {:noreply, new_state}
@@ -68,16 +149,19 @@ defmodule Job do
     Logger.debug("[Job #{inspect(self())}]: Handling task #{inspect(task_name)} failing...")
     new_ongoing_tasks = List.delete(state[:ongoing_tasks], task_name)
     disable_tasks = Planner.find_dependent_tasks(state[:enables], task_name)
+
     new_worker_pid =
       case worker_pid do
         {:fallen_worker, pid} -> pid
         pid -> pid
       end
+
     new_free_workers =
       case worker_pid do
         {:fallen_worker, _pid} -> []
         pid -> [pid]
       end
+
     Logger.debug("[Job #{inspect(self())}]: Que falta por hacer #{inspect(new_ongoing_tasks)}...")
     Logger.debug("[Job #{inspect(self())}]: Tareas a deshabilitar #{inspect(disable_tasks)}...")
 
@@ -95,17 +179,16 @@ defmodule Job do
         |> Map.put(:results, new_results)
         |> Map.put(:busy_workers, Map.delete(state[:busy_workers], new_worker_pid))
         |> Map.put(:free_workers, state[:free_workers] ++ new_free_workers)
-
       else
-
         new_plan =
-          Enum.reduce(disable_tasks, state[:plan],
-            fn d_task, acc_plan ->
-              Enum.map(acc_plan,
-                fn next_tasks ->
-                  List.delete(next_tasks, d_task)
-                end
-          ) end)
+          Enum.reduce(disable_tasks, state[:plan], fn d_task, acc_plan ->
+            Enum.map(
+              acc_plan,
+              fn next_tasks ->
+                List.delete(next_tasks, d_task)
+              end
+            )
+          end)
 
         new_plan_cleaned =
           new_plan
@@ -114,17 +197,15 @@ defmodule Job do
         Logger.debug("Nuevo plan: #{inspect(new_plan_cleaned)}")
 
         new_returns =
-          Enum.reduce(disable_tasks, state[:returns],
-            fn d_task, acc_returns ->
-              Map.put(acc_returns, d_task, :not_run)
-            end)
+          Enum.reduce(disable_tasks, state[:returns], fn d_task, acc_returns ->
+            Map.put(acc_returns, d_task, :not_run)
+          end)
           |> Map.put(task_name, {:failed, reason})
 
         new_results =
-          Enum.reduce(disable_tasks, state[:results],
-            fn d_task, acc_results ->
-              Map.put(acc_results, d_task, :not_run)
-            end)
+          Enum.reduce(disable_tasks, state[:results], fn d_task, acc_results ->
+            Map.put(acc_results, d_task, :not_run)
+          end)
           |> Map.put(task_name, {:failed, reason})
 
         Logger.debug("Tareas hechas #{inspect(new_returns)}")
@@ -137,12 +218,26 @@ defmodule Job do
         |> Map.put(:results, new_results)
         |> Map.put(:free_workers, state[:free_workers] ++ new_free_workers)
         |> Map.put(:busy_workers, Map.delete(state[:busy_workers], new_worker_pid))
-
       end
 
     should_we_finish?(new_state)
   end
 
+  @doc """
+  Handles informational messages related to worker readiness notifications.
+  This function processes messages sent to the job when a worker is ready
+  to take tasks. It updates the job state by adding the worker to the list of
+  free workers and logs the addition of the new worker.
+  # Parameters:
+  - `message`: The informational message containing the worker PID.
+  - `state`: The current state of the job, which includes ongoing tasks, free workers, and results.
+  # Returns:
+  - `{:noreply, state}`: Returns the current state of the job wrapped in a `:noreply` tuple.
+  # Example:
+  ```elixir
+  GenServer.handle_info({:notify_ready, worker_pid}, state)
+  ```
+  """
   def handle_info({:notify_ready, worker_pid}, state) do
     Logger.debug("[Job #{inspect(self())}]: Adding new worker #{inspect(worker_pid)}...")
     worker_ready(self(), worker_pid)
@@ -150,14 +245,17 @@ defmodule Job do
   end
 
   def handle_info({:DOWN, _ref, :process, pid, reason}, state) do
-
     if reason != :normal do
-      Logger.debug("[Job #{inspect(self())}]: Handling Worker #{inspect(pid)} failing because of #{inspect(reason)}")
+      Logger.debug(
+        "[Job #{inspect(self())}]: Handling Worker #{inspect(pid)} failing because of #{inspect(reason)}"
+      )
+
       task_name = Map.get(state[:busy_workers], pid)
       Logger.debug("[Job #{inspect(self())}]: Previous task being done #{inspect(task_name)}")
 
       task_completed(self(), {task_name, {:failed, reason}, {:fallen_worker, pid}})
       new_free_workers = List.delete(state[:free_workers], pid)
+
       new_state =
         state
         |> Map.put(:free_workers, new_free_workers)
@@ -166,13 +264,41 @@ defmodule Job do
     else
       {:noreply, state}
     end
-
   end
 
+  @doc """
+  Notifies the job that a task has been completed by a worker.
+  This function is called when a worker finishes executing a task.
+  It sends a message to the job process indicating that the task has been completed,
+  along with the task name, result, and worker PID. The job process will then
+  handle this message to update its state accordingly.
+  # Parameters:
+  - `job_id`: The ID of the job process that is handling the task.
+  - `task_name`: The name of the task that has been completed.
+  - `result`: The result of the task execution, which can be a success or failure.
+  - `worker_pid`: The PID of the worker that completed the task.
+  # Example:
+  ```elixir
+  Job.task_completed(job_id, {"task_1", {:result, "task_result"}, worker_pid})
+  ```
+  """
   def task_completed(job_id, {task_name, result, worker_pid}) do
     GenServer.cast(job_id, {:task_terminated, {task_name, result, worker_pid}})
   end
 
+  @doc """
+  Notifies the job that a worker is ready to take tasks.
+  This function is called when a worker signals that it is ready to receive tasks.
+  It sends a message to the job process indicating that the worker is ready,
+  allowing the job to assign tasks to the worker as they become available.
+  # Parameters:
+  - `job_pid`: The PID of the job process that is managing the workers.
+  - `worker_pid`: The PID of the worker that is ready to take tasks.
+  # Example:
+  ```elixir
+  Job.worker_ready(job_pid, worker_pid)
+  ```
+  """
   def worker_ready(job_pid, worker_pid) do
     GenServer.cast(job_pid, {:worker_ready, worker_pid})
   end
@@ -180,21 +306,25 @@ defmodule Job do
   defp should_we_finish?(state) do
     case dispatch_tasks(state) do
       :wait ->
-        if (length(Map.keys(state[:tasks])) == length(Map.keys(state[:results]))) do
+        if length(Map.keys(state[:tasks])) == length(Map.keys(state[:results])) do
           Logger.debug("[Job #{inspect(self())}]: Finished all tasks...")
           Logger.debug("[Job #{inspect(self())}]: Results #{inspect(state[:results])}")
+
           failed =
             state[:results]
             |> Map.values()
             |> Enum.any?(fn result ->
-                case result do
-                  {:failed, _reason} -> true
-                  _ -> false
-                end
-              end)
+              case result do
+                {:failed, _reason} -> true
+                _ -> false
+              end
+            end)
 
           status = if failed, do: :failed, else: :succeeded
-          Logger.debug("[Job #{inspect(self())}]: Sending to PubSub message queue #{inspect(state[:id])}")
+
+          Logger.debug(
+            "[Job #{inspect(self())}]: Sending to PubSub message queue #{inspect(state[:id])}"
+          )
 
           Phoenix.PubSub.local_broadcast(
             SPE.PubSub,
@@ -224,12 +354,17 @@ defmodule Job do
         :wait
 
       [first_tasks | next_tasks] ->
+        Logger.debug(
+          "[Job #{inspect(self())}]: This is the sequence of tasks #{inspect(state[:plan])}"
+        )
 
-        Logger.debug("[Job #{inspect(self())}]: This is the sequence of tasks #{inspect(state[:plan])}")
         free_workers = state[:free_workers]
         {to_assign, remaining_tasks} = Enum.split(first_tasks, length(free_workers))
 
-        Logger.debug("[Job #{inspect(self())}]: The are these workers #{inspect(free_workers)} available")
+        Logger.debug(
+          "[Job #{inspect(self())}]: The are these workers #{inspect(free_workers)} available"
+        )
+
         busy_workers =
           Enum.zip(to_assign, free_workers)
           |> Enum.reduce(%{}, fn {task_name, worker_pid}, acc ->
@@ -239,7 +374,10 @@ defmodule Job do
         # Empareja tareas y workers
         Enum.zip(to_assign, free_workers)
         |> Enum.each(fn {task_name, worker_pid} ->
-          Logger.debug("[Job #{inspect(self())}]: Assigning task #{inspect(task_name)} to worker #{inspect(worker_pid)}")
+          Logger.debug(
+            "[Job #{inspect(self())}]: Assigning task #{inspect(task_name)} to worker #{inspect(worker_pid)}"
+          )
+
           Worker.send_task(
             worker_pid,
             state[:id],
@@ -249,8 +387,8 @@ defmodule Job do
             state[:returns]
           )
         end)
-        Logger.debug("[Job #{inspect(self())}]: Starting tasks #{inspect(to_assign)}")
 
+        Logger.debug("[Job #{inspect(self())}]: Starting tasks #{inspect(to_assign)}")
 
         # Los workers que quedan libres después de asignar
         new_free_workers = Enum.drop(free_workers, length(to_assign))
@@ -263,16 +401,17 @@ defmodule Job do
             [remaining_tasks | next_tasks]
           end
 
-        Logger.debug("[Job #{inspect(self())}]: Remaining tasks after dispatching #{inspect(remaining_tasks)}")
+        Logger.debug(
+          "[Job #{inspect(self())}]: Remaining tasks after dispatching #{inspect(remaining_tasks)}"
+        )
+
         Logger.debug("[Job #{inspect(self())}]: New sequence of tasks #{inspect(new_plan)}")
 
         state
-          |> Map.put(:plan, new_plan)
-          |> Map.put(:ongoing_tasks, state[:ongoing_tasks] ++ to_assign)
-          |> Map.put(:free_workers, new_free_workers)
-          |> Map.put(:busy_workers, busy_workers)
-
-      end
+        |> Map.put(:plan, new_plan)
+        |> Map.put(:ongoing_tasks, state[:ongoing_tasks] ++ to_assign)
+        |> Map.put(:free_workers, new_free_workers)
+        |> Map.put(:busy_workers, busy_workers)
+    end
   end
-
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spe.MixProject do
   def project do
     [
       app: :spe,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,7 +21,8 @@ defmodule Spe.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, "~> 1.7.11"}
+      {:phoenix, "~> 1.7.11"},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
