e644b5d195d6b5d3bf24a3091a5fa0bfeb1a4f21
Night-Emperor-101 <al.ortega@alumnos.upm.es>
2025-05-30 08:25:02 +0000

    End of documentation

--- lib/superspe.ex
+++ lib/superspe.ex
@@ -1,6 +1,26 @@
 defmodule SuperSPE do
+  @moduledoc """
+  SuperSPE is the main supervisor for the SuperSPE application.
+  It manages the SuperManager and JobManager processes.
+  """
   use Supervisor
 
+  @doc """
+  Starts the SuperSPE supervisor with its children processes.
+  The children include the Phoenix PubSub, SuperManager, and JobManager.
+  The supervisor uses a one-for-one strategy with a maximum of one restart
+  within five seconds.
+  The JobManager is started with a name specified in the options.
+  The SuperManager is started without any options.
+  The PubSub is started with a name defined in the SPE module.
+  The supervisor is named SPE.SuperSPE.
+  #### Returns:
+  - `{:ok, pid}`: The Supervisor's PID is returned, indicating that it has been successfully started.
+  #### Example:
+  ```elixir
+  {:ok, sup_pid} = SuperSPE.start_link([])
+  ```
+  """
   @impl Supervisor
   def init(children) do
     opts = [
@@ -12,6 +32,18 @@ defmodule SuperSPE do
     Supervisor.init(children, opts)
   end
 
+  @doc """
+  Starts the SuperSPE supervisor with the given options.
+  This function initializes the Supervisor and sets it up to manage the SuperManager and JobManager processes.
+  #### Parameters:
+  - `opts`: Options for the JobManager, including the name to be used.
+  #### Returns:
+  - `{:ok, pid}`: The Supervisor's PID is returned, indicating that it has been successfully started.
+  #### Example:
+  ```elixir
+  {:ok, sup_pid} = SuperSPE.start_link(name: SPE.JobManager)
+  ```
+  """
   def start_link(opts) do
     pubsub = Phoenix.PubSub.child_spec(name: SPE.PubSub)
 
